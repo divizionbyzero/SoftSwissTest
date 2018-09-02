34fd761c287abaef98dfe07dfcfad739b06cd5ce
Alena Kernazhytskaya <divizionbyzero@bk.ru>
2018-09-02 16:14:42 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Test project for SoftSwiss company
+
+### Installation
+
+Project requires [Node.js](https://nodejs.org/) to run.
+
+You need Gulp installed globally:
+
+```sh
+$ npm i -g gulp
+```
+
+```sh
+$ git clone [https://github.com/divizionbyzero/SoftSwissTest.git]
+$ cd SoftSwissTest
+$ npm i -d
+$ gulp watch
+```
+
+After all dependencies will be install, and watcher is started, you can run index.html, see and change my test project
+
+### Demo
+You can see the result of my work [here](https://s3-us-west-2.amazonaws.com/alena.kernazhytskaya/soft-swiss-test/index.html)
+
+### Tech
+
+This project uses a number of open source projects to work properly:
+
+* [AngularJS](https://angularjs.org/) - HTML enhanced for web apps!
+* [Gulp](http://gulpjs.com/) - the streaming build system
+* [Jade](http://jade-lang.com/) - Node template engine
+* [Stylus](http://stylus-lang.com/) - CSS preprocessor
+
+
+### Plugins
+
+Project is currently extended with the following plugins
+
+* [ng-dialog](https://github.com/likeastore/ngDialog)
---
+++ frontend/scripts/scripts.js
@@ -0,0 +1,113 @@
+(function () {
+
+    var app = angular.module("testTask", ['ngDialog']);
+
+    app.controller('productsCtrl', ['$scope', 'ngDialog', function ($scope, ngDialog) {
+        $scope.data = {
+            "items": [
+                {
+                    "type" : "Child",
+                    "img" : "item1",
+                    "price" : "3.20",
+                    "name" : "T-SHIRT",
+                    "options": ["Size", "S", "M", "L", "XL"]
+                }
+                ,{
+                    "type" : "Child",
+                    "img" : "item2",
+                    "price" : "13.30",
+                    "name" : "Pants FORCLAZ",
+                    "options": ["Size", "M", "XL"]
+                }
+                ,{
+                    "type" : "Men",
+                    "img" : "item1",
+                    "price" : "5.00",
+                    "name" : "T-SHIRT",
+                    "options": ["Size", "XL"]
+                }
+                ,{
+                    "type" : "Women",
+                    "img" : "item2",
+                    "price" : "3.21",
+                    "name" : "T-SHIRT",
+                    "options":  ["Size", "S", "M", "L", "XL"]
+                }
+                ,{
+                    "type" : "Women",
+                    "img" : "item3",
+                    "price" : "31",
+                    "name" : "backpack",
+                    "options": ["color", "red", "blue", "black"]
+                }
+                , {
+                    "type" : "Other",
+                    "img" : "item3",
+                    "price" : "0",
+                    "name" : "car",
+                    "options": ["color", "red", "blue", "black"]
+                }
+            ]
+        };
+
+        $scope.showCategory = {
+            "men" : true,
+            "women" : true,
+            "child": false
+        };
+
+        $scope.changeFilter  = function (obj) {
+            return $scope.showCategory[obj.type.toLowerCase()];
+        };
+
+        $scope.resetFilters = function () {
+            Object.keys($scope.showCategory).forEach(function(key) {
+                $scope.showCategory[key] = true;
+            });
+        };
+
+        $scope.clickToOpen = function (item) { //open modal window by click on product photo
+            if (screen.width > 768)
+                ngDialog.open({
+                    template: '<div class="popup__wrap">'
+                        + '<img class="popup__img" ng-src="images/products_photos/'
+                        + item.img + '.png" alt=""/><span class="popup__name">'
+                        + item.name + '</span>',
+                    className: 'ngdialog-theme-default',
+                    plain: true,
+                    width: '60%' });
+        };
+    }]);
+
+    app.controller('menuCtrl', ['$scope', function ($scope) {
+
+        $scope.data = {
+            "footerMenu" : [
+                {
+                    "name": "Credits",
+                    "link": "https://softswiss.com/",
+                    "title": "credits"
+                },
+                {
+                    "name": "Privacy",
+                    "link": "https://softswiss.com/",
+                    "title": "privacy"
+                },
+                {
+                    "name": "About",
+                    "link": "https://www.softswiss.com/about-us/",
+                    "title": "about us"
+                },
+                {
+                    "name": "Contact",
+                    "link": "https://www.softswiss.com/contact-us/",
+                    "title": "contact us"
+                }
+            ]};
+
+    }]);
+
+})();
+
+
+
